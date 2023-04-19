810b90428926b2d82de2428b5399cc5b67ff8f62
Isaac Amortegui <isaacamortegc@outlook.com>
2023-04-19 18:14:55 -0500

    Creation of Grass and Rose classes

---
+++ Rose.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalHierarchyLibrary
+{
+    public class Rose : Herbal
+    {
+        public override string GetSpeciesName()
+        {
+            return "Rose";
+        }
+    }
+}
